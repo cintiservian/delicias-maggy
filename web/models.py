2b100d924d599fd262878a9243a5ac2d67f5e5be
Cintia Servian <cintiamargot.servian@gmail.com>
2024-08-01 01:55:16 -0300

    implementacion modelo tartas

--- web/models.py
+++ web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class tartas(models.Model):
+    tartas_uuid= models.UUIDField() #del tipo UUIDField
+    name= models.CharField (max_length=64)   #del tipo CharField (largo máximo 64 caracteres)
+    description= models.TextField()
+    image_url= models.URLField()   # del tipo URLField
+    slug= models.SlugField()  # del tipo SlugField
+    is_private= models.BooleanField()  # del tipo BooleanField
+
+    def _str_(self):
+        return self.name
+    
--- web/views.py
+++ web/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import tartas
 
 # Create your views here.
 def inicio(request):
-    return render(request,"index.html")
+    mistartas=tartas.objects.filter(is_private=False)
+    return render(request,'index.html',{'tartaletas':mistartas})
 
 def acerca (request):
     return render (request,"acerca.html")
 
 def bienvenido (request):
-    return render (request,"bienvenido.html")
+    tartas_privados = tartas.objects.filter(is_private=True)
+    return render (request,"bienvenido.html",{'tartaletas':tartas_privados})
